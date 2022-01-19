5703099f632a13736460bbf3cfaa783749b186af
Dylan Jones <dylan@dylanjones.io>
2022-01-18 19:51:00 -0700

    Added problem 5 solution in Rust.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,6 +4,7 @@ mod prob1;
 // mod prob2;
 mod prob3;
 mod prob4;
+mod prob5;
 
 fn main() {
     println!("Run `cargo test` for checking solutions.");
---
+++ rust/src/prob5.rs
@@ -0,0 +1,32 @@
+pub fn longest_palindrome(s: String) -> String {
+    fn is_palindrome(s: &str) -> bool {
+        s.as_bytes().iter().eq(s.as_bytes().iter().rev())
+    }
+    if s.len() < 2 {
+        return s;
+    }
+    let total_len = s.len();
+    for length in (1..=s.len()).rev() {
+        for i in 0..=(total_len - length) {
+            let substr = &s[i..i + length];
+            if is_palindrome(substr) {
+                return substr.to_string();
+            }
+        }
+    }
+    "".to_string()
+}
+
+#[cfg(test)]
+mod problem_tests {
+    use super::*;
+
+    #[test]
+    fn test_longest_palindrome() {
+        assert_eq!(longest_palindrome("babad".to_string()), "bab".to_string());
+        assert_eq!(longest_palindrome("cbbd".to_string()), "bb".to_string());
+        assert_eq!(longest_palindrome("".to_string()), "".to_string());
+        assert_eq!(longest_palindrome("a".to_string()), "a".to_string());
+        assert_eq!(longest_palindrome("ac".to_string()), "a".to_string());
+    }
+}
