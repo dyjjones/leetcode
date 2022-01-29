09e3775f83023369bb2f5d6607c42f9f88378a71
Dylan Jones <dylan@dylanjones.io>
2022-01-28 20:28:00 -0700

    Added problem 11 solution in Rust.

---
+++ rust/src/prob11.rs
@@ -0,0 +1,76 @@
+// fn advance_by<'a, I>(mut it: I, n: &i32) -> I
+// where
+//     I: Iterator<Item = (usize, &'a i32)>,
+// {
+//     for _ in 0..*n {
+//         it.next();
+//     }
+//     it
+// }
+
+// struct Max {
+//     index: usize,
+//     val: i32,
+// }
+
+// #[derive(PartialEq, Eq, Debug)]
+// enum Side {
+//     Left,
+//     Right,
+// }
+// impl Side {
+//     fn flip(&mut self) {
+//         *self = match self {
+//             Self::Left => Self::Right,
+//             Self::Right => Self::Left,
+//         }
+//     }
+// }
+
+pub fn max_area(height: Vec<i32>) -> i32 {
+    let mut max = 0;
+    let mut left = 0_usize;
+    let mut right = height.len() - 1;
+
+    // outside loop for first vals
+    let mut height_left = height[left];
+    let mut height_right = height[right];
+    let mut current_area = (right - left) as i32 * height_left.min(height_right);
+    if max < current_area {
+        max = current_area;
+    }
+
+    loop {
+        // move shorter one over
+        if height_left < height_right {
+            // move left to the right
+            left += 1;
+        } else {
+            // move right to the left
+            right -= 1;
+        }
+
+        if right <= left {
+            break;
+        }
+
+        height_left = height[left];
+        height_right = height[right];
+        current_area = (right - left) as i32 * height_left.min(height_right);
+        if max < current_area {
+            max = current_area;
+        }
+    }
+    max
+}
+
+#[cfg(test)]
+mod problem_tests {
+    use super::*;
+
+    #[test]
+    fn test_max_area() {
+        assert_eq!(max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]), 49);
+        assert_eq!(max_area(vec![1, 1]), 1);
+    }
+}
