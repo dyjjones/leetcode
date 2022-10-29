2b30402a0e43496561c89671bcc2bdb4e424380d
Dylan Jones <dylan@dylanjones.io>
2022-10-29 13:00:22 -0700

    Formatting.

--- python/leetcode/problems/p1.py
+++ python/leetcode/problems/p1.py
@@ -1,6 +1,7 @@
 from typing import List
 from collections import defaultdict
 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         num_indices = defaultdict(list)
